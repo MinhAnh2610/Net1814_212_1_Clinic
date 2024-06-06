0f98638ba76167afec14dc043e93084df24ead71
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-06-06 08:12:34 +0700

    Resolve conflict

--- Clinic.Data/Models/Net1814_212_1_ClinicContext.cs
+++ Clinic.Data/Models/Net1814_212_1_ClinicContext.cs
@@ -30,10 +30,10 @@ public partial class Net1814_212_1_ClinicContext : DbContext
     public virtual DbSet<RecordDetail> RecordDetails { get; set; }
 
     public virtual DbSet<Service> Services { get; set; }
-    private const string Phuc = "Data Source=PHUC\\PHUC;Initial Catalog=Net1814_212_1_Clinic;User ID=sa;Password=123456789;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer(Phuc);
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-3ED3JRS\\SQLEXPRESS;Initial Catalog=Net1814_212_1_Clinic;Persist Security Info=True;User ID=soybean;Password=123456;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
