15fee2257a272c9554b1d7f99179a3d0881d76d5
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-06-16 12:38:49 +0700

    Modify WCustoner and Code-behind

--- Clinic.WpfApp/UI/WCustomer.xaml.cs
+++ Clinic.WpfApp/UI/WCustomer.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Clinic.Business.Base;
 using Clinic.Business.Clinic;
 using Clinic.Data.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -27,8 +29,27 @@ namespace Clinic.WpfApp.UI
         public WCustomer()
         {
             InitializeComponent();
+            ResizeMode = ResizeMode.CanResize;
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            // Handle the window state change event
+            WindowState = WindowState.Maximized;
+            StateChanged += MainWindow_StateChanged;
             _customerBusiness = new CustomerBusiness();
             LoadCustomers();
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+            {
+                // Hide the taskbar when the window is maximized
+                WindowStyle = WindowStyle.None;
+            }
+            else
+            {
+                // Show the taskbar when the window is not maximized
+                WindowStyle = WindowStyle.SingleBorderWindow;
+            }
         }
 
         private async void LoadCustomers()
@@ -51,31 +72,86 @@ namespace Clinic.WpfApp.UI
             }
         }
 
+        private async void LoadCustomers(IEnumerable<Customer> customers)
+        {
+            try
+            {
+                //if empty list, load default list
+                if(customers.IsNullOrEmpty())
+                {
+                    LoadCustomers();
+                    return;
+
+                }
+                customerList.ItemsSource = customers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private async void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string customerId = CustomerId.Text.Trim();
+            string firstName = CustomerFirstName.Text.Trim();
+            string lastName = CustomerLastName.Text.Trim();
+            string phone = CustomerPhone.Text.Trim();
+            string address = CustomerAddress.Text.Trim();
+            string email = CustomerEmail.Text.Trim();
+            string dob = CustomerDoB.Text.Trim();
+            string createdAt = CustomerCreatedAt.Text.Trim();
+            var bResult = await _customerBusiness.GetAll();
+            var customerList = bResult.Data as List<Customer>;
+           
+            // Create a query to filter the customers based on the search criteria
+            var query = from customer in customerList
+                        where (string.IsNullOrEmpty(customerId) || customer.CustomerId.ToString() == customerId)
+                        && (string.IsNullOrEmpty(firstName) || customer.FirstName.Contains(firstName))
+                        && (string.IsNullOrEmpty(lastName) || customer.LastName.Contains(lastName))
+                        && (string.IsNullOrEmpty(phone) || customer.Phone.Contains(phone))
+                        && (string.IsNullOrEmpty(address) || customer.Address.Contains(address))
+                        && (string.IsNullOrEmpty(email) || customer.Email.Contains(email))
+                        && (string.IsNullOrEmpty(dob) || customer.DoB.ToString() == dob)
+                        && (string.IsNullOrEmpty(createdAt) || customer.CreatedAt.ToString() == createdAt)
+                        select customer;
+
+            // Bind the results to a data source (e.g., a DataGridView)
+            LoadCustomers(query.ToList());
+        }
+
         private async void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 //check if customer id already existed
                 var item = await _customerBusiness.GetById(Int32.Parse(CustomerId.Text));
-                
 
+                
                 //case : update
                 if(item.Data != null)
                 {
+                    ComboBoxItem comboBoxGender = (ComboBoxItem)comboGender.SelectedItem;
+                    string gender = comboBoxGender.Content.ToString();
+                    ComboBoxItem comboBoxIsActive = (ComboBoxItem)comboIsActive.SelectedItem;
+                    string isActive = comboBoxIsActive.Content.ToString();
                     var customer = item.Data as Customer;
                     customer.CustomerId = Int32.Parse(CustomerId.Text);
                     customer.FirstName = CustomerFirstName.Text;
                     customer.LastName = CustomerLastName.Text;
                     customer.Phone = CustomerPhone.Text;
+                    customer.Address = CustomerAddress.Text;
+                    customer.Gender = (gender == "Male") ? true : false;
+                    customer.Email = CustomerEmail.Text;
+                    customer.DoB = DateOnly.Parse(CustomerDoB.Text);
+                    customer.CreatedAt = (CustomerCreatedAt.Text == string.Empty) ? DateOnly.FromDateTime(DateTime.Now) : DateOnly.Parse(CustomerCreatedAt.Text);
+                    customer.IsActive = (isActive == "True") ? true : false;
 
                     var result = await _customerBusiness.Update(customer);
                     MessageBox.Show(result.Message, "Update");
 
                     //reset text box
-                    CustomerId.Text = string.Empty;
-                    CustomerFirstName.Text = string.Empty;
-                    CustomerLastName.Text = string.Empty;
-                    CustomerPhone.Text = string.Empty;
+                    RefreshTextBox();
 
                     //refresh list
                     LoadCustomers();
@@ -83,22 +159,29 @@ namespace Clinic.WpfApp.UI
                 }
                 else
                 {
+                    ComboBoxItem comboBoxGender = (ComboBoxItem)comboGender.SelectedItem;
+                    string gender = comboBoxGender.Content.ToString();
+                    ComboBoxItem comboBoxIsAtive = (ComboBoxItem)comboIsActive.SelectedItem;
+                    string isActive = comboBoxIsAtive.Content.ToString();
                     var customer = new Customer()
                     {
                         CustomerId = Int32.Parse(CustomerId.Text),
                         FirstName = CustomerFirstName.Text,
                         LastName = CustomerLastName.Text,
-                        Phone = CustomerPhone.Text
+                        Phone = CustomerPhone.Text,
+                        Address = CustomerAddress.Text,
+                        Gender = (gender == "Male") ? true : false,
+                        Email = CustomerEmail.Text,
+                        DoB = DateOnly.Parse(CustomerDoB.Text),
+                        CreatedAt = (CustomerCreatedAt.Text == string.Empty) ? DateOnly.FromDateTime(DateTime.Now) : DateOnly.Parse(CustomerCreatedAt.Text),
+                        IsActive = (isActive == "True") ? true : false
                     };
 
                     var result = await _customerBusiness.Save(customer);
                     MessageBox.Show(result.Message, "Save");
 
                     //reset text box
-                    CustomerId.Text = string.Empty;
-                    CustomerFirstName.Text = string.Empty;
-                    CustomerLastName.Text = string.Empty;
-                    CustomerPhone.Text = string.Empty;
+                    RefreshTextBox();
 
                     //refresh list
                     LoadCustomers();
@@ -114,6 +197,18 @@ namespace Clinic.WpfApp.UI
             {
                 MessageBox.Show(ex.ToString(), "Error");
             }
+        }
+
+        private void RefreshTextBox()
+        {
+            CustomerId.Text = string.Empty;
+            CustomerFirstName.Text = string.Empty;
+            CustomerLastName.Text = string.Empty;
+            CustomerPhone.Text = string.Empty;
+            CustomerAddress.Text = string.Empty;
+            CustomerEmail.Text = string.Empty;
+            CustomerDoB.Text = string.Empty;
+            CustomerCreatedAt.Text = string.Empty;
         }
 
         private async void ButtonDelete_Click(object sender, RoutedEventArgs e)
@@ -139,7 +234,7 @@ namespace Clinic.WpfApp.UI
                     MessageBox.Show(result.Message, "Delete");
                 }
                 //refresh list
-                LoadCustomers();
+                LoadCustomers();    
             }
             catch(Exception ex)
             {
@@ -151,10 +246,7 @@ namespace Clinic.WpfApp.UI
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
             //reset text box
-            CustomerId.Text = string.Empty;
-            CustomerFirstName.Text = string.Empty;
-            CustomerLastName.Text = string.Empty;
-            CustomerPhone.Text = string.Empty;
+            RefreshTextBox();
         }
 
         private async void grdCustomer_MouseDouble_Click(object sender, MouseEventArgs e)
@@ -169,15 +261,21 @@ namespace Clinic.WpfApp.UI
                     var item = row.Item as Customer;
                     if(item != null)
                     {
-                        var currencyResult = await _customerBusiness.GetById(item.CustomerId);
+                        var customerResult = await _customerBusiness.GetById(item.CustomerId);
 
-                        if(currencyResult.Status > 0 && currencyResult.Data != null)
+                        if(customerResult.Status > 0 && customerResult.Data != null)
                         {
-                            item = currencyResult.Data as Customer;
+                            item = customerResult.Data as Customer;
                             CustomerId.Text = item.CustomerId.ToString();
                             CustomerFirstName.Text = item.FirstName;
                             CustomerLastName.Text = item.LastName;
                             CustomerPhone.Text = item.Phone;
+                            CustomerAddress.Text = item.Address;
+                            comboGender.SelectedIndex = (item.Gender) ? 0 : 1;
+                            CustomerEmail.Text = item.Email;
+                            CustomerDoB.Text = item.DoB.ToString();
+                            CustomerCreatedAt.Text = item.CreatedAt.ToString();
+                            comboIsActive.SelectedIndex = (item.IsActive) ? 0 : 1;
                         }
                     }
                 }
